a97f7b8905ecea08fc90309a04f0be2e76de1f72
Javier Beneito Barquero <jbbarquero@gmail.com>
2018-06-08 07:53:05 +0200

    netty-intro: project with java 8 and dependencies.

--- src/main/java/com/malsolo/netty/App.java
+++ src/main/java/com/malsolo/netty/App.java
@@ -1,13 +1,37 @@
 package com.malsolo.netty;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+import static java.util.Comparator.*;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main( String[] args ) {
+        Arrays.asList(
+                new Person(5, "Niña", "W"),
+                new Person(20, "Chica", "W"),
+                new Person(30, "Señor, la pelota", "M"),
+                new Person(30, "Señoraaaa", "W")
+        ).stream()
+                .filter(p -> "W".equals(p.getGender()))
+                .sorted(comparing(Person::getAge))
+                .map(Person::getName)
+                .forEach(System.out::println);
+
         System.out.println( "Hello World!" );
     }
+}
+
+@Data
+class Person {
+    private final int age;
+    private final String name;
+    private final String gender;
 }
